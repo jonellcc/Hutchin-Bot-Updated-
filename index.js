cb1801258f6632c6c360a8d4f1f28c99de837b71
CHORU-OFFICIAL <112081055+ChoruTiktokers182@users.noreply.github.com>
2025-05-06 07:13:25 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+@ChoruOfficial
+#* Btw this template is default only *#
+#* You can add your own template, but it must be well-made so that you can add us to the exocore organization. We are available on Facebook. *#
+#* You are not allowed to use express package nodejs here instead use const { app } = require("../exocore-web") *#
+*/
+
+const chalk = require('chalk');
+const logo = (name) => {
+  const art = `
+          _                       
+    /\\   | |                      
+   /  \\  | | __ _ _   _  ___ _ __ 
+  / /\\ \\ | |/ _\` | | | |/ _ \\ '__|
+ / ____ \\| | (_| | |_| |  __/ |   
+/_/    \\_\\_|\\__,_|\\__, |\\___|_|   
+                   __/ |         
+                  |___/         
+
+       [ ${name} Project Started ]
+  `;
+  console.log(chalk.cyan(art));
+};
+
+logo("Ayaka");
