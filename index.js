9fd3fbda2c6d7455e7cbb8bf6fe996a68b7f8dc8
CHORU-OFFICIAL <112081055+ChoruTiktokers182@users.noreply.github.com>
2025-05-06 07:15:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /*
-@ChoruOfficial
+@JohnSteveCostaños as known @ChoruOfficial
 #* Btw this template is default only *#
 #* You can add your own template, but it must be well-made so that you can add us to the exocore organization. We are available on Facebook. *#
 #* You are not allowed to use express package nodejs here instead use const { app } = require("../exocore-web") *#
+#* Reminder, change the package.json, replace your name in the author and you can also change the package name. *#
 */
 
 const chalk = require('chalk');
