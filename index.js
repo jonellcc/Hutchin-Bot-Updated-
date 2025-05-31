420dc6a408e45bb21b6b185b310721fbb5345f8a
CHORU-OFFICIAL <112081055+ChoruTiktokers182@users.noreply.github.com>
2025-05-31 14:37:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 /*
 @JohnSteveCostaños as known @ChoruOfficial
-#* Btw this template is default only *#
-#* You can add your own template, but it must be well-made so that you can add us to the exocore organization. We are available on Facebook. *#
-#* You are not allowed to use express package nodejs here instead use const { app } = require("../exocore-web") *#
-#* Reminder, change the package.json, replace your name in the author and you can also change the package name. *#
-*/
+#* Btw this template is default only *#*/
+//# Doesn't need express
+module.exports = function(router) {
+  router.get('/', (req, res) => {
+    res.send('OwnersV2 lel');
+  });
 
-const chalk = require('chalk');
-const logo = (name) => {
-  const art = `
-          _                       
-    /\\   | |                      
-   /  \\  | | __ _ _   _  ___ _ __ 
-  / /\\ \\ | |/ _\` | | | |/ _ \\ '__|
- / ____ \\| | (_| | |_| |  __/ |   
-/_/    \\_\\_|\\__,_|\\__, |\\___|_|   
-                   __/ |         
-                  |___/         
-
-       [ ${name} Project Started ]
-  `;
-  console.log(chalk.cyan(art));
+  router.get('/:id', (req, res) => {
+    res.send(`Product ${req.params.id} from OwnersV2`);
+  });
 };
-
-logo("Ayaka");
