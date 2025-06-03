53d890f9e93232acc149014865fdd0cb21d926bb
CHORU-OFFICIAL <112081055+ChoruTiktokers182@users.noreply.github.com>
2025-06-04 05:53:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
+const { generateAndSaveRoutes } = require("./pathsRoutes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
