17a62c9177355c0b8f52da8592d5c77b7b38ef8a
CHORU-OFFICIAL <112081055+ChoruTiktokers182@users.noreply.github.com>
2025-06-03 17:42:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,21 @@
 #Btw this template is default only 
 */
 
-const fs = require('fs');
-const data = { data: 'HELLO WORLDS' };
-fs.writeFileSync('./express-data.json', JSON.stringify(data, null, 2));
+const express = require('express');
+const app = express(); // Ito 'yung 'app' instance na ipapasa natin
+const PORT = 3000;
+const saveDetectedRoutes = require('./pathsRoutes');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+  res.send('Love Server Root - Updated!');
+});
+
+app.on('error', (error) => {
+  console.error('[Server] Server error:', error);
+  if (error.code === 'EADDRINUSE') {
+    console.error(`[Love Server] Port ${PORT} is already in use.`);
+    process.exit(1);
+  }
+});
